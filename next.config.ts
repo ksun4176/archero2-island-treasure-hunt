d52d05174754f2fd1d3ec95550ed8242f3654922
unknown <ksun4176@gmail.com>
2025-04-14 22:14:46 -0400

    add nextjs

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  basePath: "/archero2-island-treasure-hunt",
+  output: "export",
+};
+
+export default nextConfig;
