101a8ae22203e2bf7ee371cb4969b5287875c46d
unknown <ksun4176@gmail.com>
2025-09-20 12:46:54 -0400

    retire page and redirect to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,130 @@
-"use client"
-import MultiplierMapContent from "@/components/MultiplierMapContent";
-import WhereAreDiceContent from "@/components/WhereAreDiceContent";
-import ShouldRollContent from "@/components/ShouldRollContent";
-import PpidCalcContent from "@/components/PpidCalculatorContent";
-import TipsForNewContent from "@/components/TipsForNewContent";
-import FaqContent from "@/components/FaqContent";
-import { IconButton } from "@mui/material";
-import Drawer from "@mui/material/Drawer";
+'use client';
+import MultiplierMapContent from '@/components/MultiplierMapContent';
+import WhereAreDiceContent from '@/components/WhereAreDiceContent';
+import ShouldRollContent from '@/components/ShouldRollContent';
+import PpidCalcContent from '@/components/PpidCalculatorContent';
+import TipsForNewContent from '@/components/TipsForNewContent';
+import FaqContent from '@/components/FaqContent';
+import { IconButton } from '@mui/material';
+import Drawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
-import Image from "next/image";
-import { JSX, useState } from "react";
-import { basePath } from "@/utils/constants";
+import Image from 'next/image';
+import { JSX, useState } from 'react';
+import { basePath } from '@/utils/constants';
 
 type Section = {
   href: string;
   label: string;
   heading: React.ReactNode;
   content?: JSX.Element;
-}
+};
 
 const sections: Section[] = [
   {
-    href: "tips-for-newbies",
-    label: "Tips for Newbies",
-    heading: <>Tips for Newbies <span className="whitespace-nowrap">◕⩊◕</span></>,
+    href: 'tips-for-newbies',
+    label: 'Tips for Newbies',
+    heading: (
+      <>
+        Tips for Newbies <span className='whitespace-nowrap'>◕⩊◕</span>
+      </>
+    ),
     content: <TipsForNewContent />,
   },
+  // {
+  //   href: 'where-are-the-dice',
+  //   label: 'Where are the dice?!',
+  //   heading: (
+  //     <>
+  //       Where are the dice?!{' '}
+  //       <span className='whitespace-nowrap'>(╯°□°)╯︵ ┻━┻</span>
+  //     </>
+  //   ),
+  //   content: <WhereAreDiceContent />,
+  // },
+  // {
+  //   href: 'multiplier-map',
+  //   label: 'Multiplier Map',
+  //   heading: (
+  //     <>
+  //       Multiplier Map <span className='whitespace-nowrap'>( • ̀ω•́ )✧</span>
+  //     </>
+  //   ),
+  //   content: <MultiplierMapContent />,
+  // },
+  // {
+  //   href: 'should-i-roll',
+  //   label: 'Should I roll?',
+  //   heading: (
+  //     <>
+  //       Should I roll? <span className='whitespace-nowrap'>¯\\_(ツ)_/¯</span>
+  //     </>
+  //   ),
+  //   content: <ShouldRollContent />,
+  // },
+  // {
+  //   href: 'ppid-calculator',
+  //   label: 'PPID Calculator',
+  //   heading: (
+  //     <>
+  //       Points per Dice Calculator{' '}
+  //       <span className='whitespace-nowrap'>◁ |⚙⌨⚙| ▷</span>
+  //     </>
+  //   ),
+  //   content: <PpidCalcContent />,
+  // },
   {
-    href: "where-are-the-dice",
-    label: "Where are the dice?!",
-    heading: <>Where are the dice?! <span className="whitespace-nowrap">(╯°□°)╯︵ ┻━┻</span></>,
-    content: <WhereAreDiceContent />,
-  },
-  {
-    href: "multiplier-map",
-    label: "Multiplier Map",
-    heading: <>Multiplier Map <span className="whitespace-nowrap">( • ̀ω•́ )✧</span></>,
-    content: <MultiplierMapContent />,
-  },
-  {
-    href: "should-i-roll",
-    label: "Should I roll?",
-    heading: <>Should I roll? <span className="whitespace-nowrap">¯\\_(ツ)_/¯</span></>,
-    content: <ShouldRollContent />,
-  },
-  {
-    href: "ppid-calculator",
-    label: "PPID Calculator",
-    heading: <>Points per Dice Calculator <span className="whitespace-nowrap">◁ |⚙⌨⚙| ▷</span></>,
-    content: <PpidCalcContent />,
-  },
-  {
-    href: "faq",
-    label: "FAQs",
-    heading: <>FAQs <span className="whitespace-nowrap">(º～º)</span></>,
+    href: 'faq',
+    label: 'FAQs',
+    heading: (
+      <>
+        FAQs <span className='whitespace-nowrap'>(º～º)</span>
+      </>
+    ),
     content: <FaqContent />,
-  }
-]
+  },
+];
 
 export default function Home() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  // const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const toggleSidebar = (newOpen: boolean) => () => {
-    setSidebarOpen(newOpen);
-  };
+  // const toggleSidebar = (newOpen: boolean) => () => {
+  //   setSidebarOpen(newOpen);
+  // };
 
-return (
-    <div className="flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <div className="flex items-center gap-2 p-2 bg-gray-300 dark:bg-gray-600 sticky top-0 z-1">
-        <div className="grow-0 md:hidden">
+  return (
+    <div className='flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]'>
+      <div className='flex items-center gap-2 p-2 bg-gray-300 dark:bg-gray-600 sticky top-0 z-1'>
+        {/* <div className='grow-0 md:hidden'>
           <IconButton
-            size="small"
-            aria-label="open sidebar"
+            size='small'
+            aria-label='open sidebar'
             onClick={toggleSidebar(!sidebarOpen)}
           >
             <MenuIcon />
           </IconButton>
-        </div>
-        <div className="grow-0">
+        </div> */}
+        <div className='grow-0'>
           <Image
-            className="w-[36px]"
+            className='w-[36px]'
             src={`${basePath}/diceicon.png`}
-            alt="Island Treasure Hunt"
+            alt='Island Treasure Hunt'
             height={177}
             width={151}
             unoptimized
           />
         </div>
-        <div className="flex-1 flex flex-col md:flex-row md:items-center md:gap-1">
-          <h1 className="font-semibold text-2xl">Island Treasure Hunt</h1>
-          <p className="text-sm">by Kaithulhu</p>
+        <div className='flex-1 flex flex-col md:flex-row md:items-center md:gap-1'>
+          <h1 className='font-semibold text-2xl'>Island Treasure Hunt</h1>
         </div>
-        <div className="hidden md:block">
-          <a href='https://ko-fi.com/O4O71FBM0I' target='_blank'>
+        <div className='hidden md:block'>
+          <a
+            href='https://ko-fi.com/O4O71FBM0I'
+            target='_blank'
+          >
             <Image
               height={36}
               width={143}
-              className="h-[36px] w-[143px] border-0"
+              className='h-[36px] w-[143px] border-0'
               src='https://storage.ko-fi.com/cdn/kofi5.png?v=6'
               alt='Buy Me a Coffee at ko-fi.com'
               unoptimized
@@ -105,70 +133,93 @@ return (
         </div>
       </div>
       {/* Sidebar */}
-      <Drawer open={sidebarOpen} onClose={toggleSidebar(false)}>
-        <div className="p-2">
-          <div className="text-xl font-semibold">Table of content</div>
-          <ul className="mt-2 list-disc pr-2 pl-6">
-            {sections.map(section => 
+      {/* <Drawer
+        open={sidebarOpen}
+        onClose={toggleSidebar(false)}
+      >
+        <div className='p-2'>
+          <div className='text-xl font-semibold'>Table of content</div>
+          <ul className='mt-2 list-disc pr-2 pl-6'>
+            {sections.map((section) => (
               <li key={section.href}>
-                <a 
-                  className="block hover:bg-gray-200 dark:hover:bg-gray-900 px-2 py-1 rounded"
+                <a
+                  className='block hover:bg-gray-200 dark:hover:bg-gray-900 px-2 py-1 rounded'
                   href={`#${section.href}`}
                   onClick={toggleSidebar(false)}
                 >
                   {section.label}
                 </a>
               </li>
-            )}
+            ))}
           </ul>
         </div>
-      </Drawer>
-      <div className="flex items-start h-full gap-2 py-2 md:px-2">
+      </Drawer> */}
+      <div className='flex flex-col items-center h-full gap-2 py-2 md:px-2'>
         {/* Table of Content */}
-        <div className="grow-0 bg-gray-100 dark:bg-gray-800 p-2 rounded sticky top-13 hidden md:block">
-          <div className="text-xl font-semibold">Table of content</div>
-          <ul className="mt-2 list-disc pr-2 pl-6">
-            {sections.map(section => 
+        {/* <div className='grow-0 bg-gray-100 dark:bg-gray-800 p-2 rounded sticky top-13 hidden md:block'>
+          <div className='text-xl font-semibold'>Table of content</div>
+          <ul className='mt-2 list-disc pr-2 pl-6'>
+            {sections.map((section) => (
               <li key={section.href}>
                 <a
-                  className="block hover:bg-gray-200 dark:hover:bg-gray-900 px-2 py-1 rounded"
+                  className='block hover:bg-gray-200 dark:hover:bg-gray-900 px-2 py-1 rounded'
                   href={`#${section.href}`}
                 >
                   {section.label}
                 </a>
               </li>
-            )}
+            ))}
           </ul>
-        </div>
-        {/* Content */}
-        {/* <div className="flex-1 flex flex-col h-full">
-          <div className="text-3xl p-2 bg-gray-100 dark:bg-gray-800 ">
-            {"Down until VIP Discord access is given to a few key community helpers to ensure those who contribute and care about the community have a direct line to the developers."}
-          </div>
         </div> */}
-        <div className="flex-1 flex flex-col h-full">
-          <div className="p-2 bg-gray-100 dark:bg-gray-800 ">
-            {sections.map((section, index) =>
-              <div id="container" key={index} className="w-full flex flex-col mb-8">
+        {/* Content */}
+        <div className='text-xl font-semibold p-2 bg-yellow-100 dark:bg-yellow-700'>
+          <p>
+            This page is no longer being maintained so information might be
+            outdated. Last updated July 2025.
+          </p>
+          <p>
+            Check out the new{' '}
+            <a
+              href='https://ksun4176.github.io/archero2-dice-companion/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-600 dark:text-blue-400 underline'
+            >
+              Archero2 Dice Companion with Guide
+            </a>{' '}
+            instead.
+          </p>
+        </div>
+        <div className='flex-1 flex flex-col h-full'>
+          <div className='p-2 bg-gray-100 dark:bg-gray-800 '>
+            {sections.map((section, index) => (
+              <div
+                id='container'
+                key={index}
+                className='w-full flex flex-col mb-8'
+              >
                 <h2
                   id={section.href}
-                  className="font-semibold text-2xl border-b border-gray-500 mb-1 -mt-18 pt-18 md:-mt-13 md:pt-13 pointer-events-none"
+                  className='font-semibold text-2xl border-b border-gray-500 mb-1 -mt-18 pt-18 md:-mt-13 md:pt-13 pointer-events-none'
                 >
                   {section.heading}
                 </h2>
                 {section.content}
               </div>
-            )}
+            ))}
           </div>
         </div>
       </div>
       {/* Footer */}
-      <div className="flex justify-end p-2 block md:hidden sticky bottom-0">
-        <a href='https://ko-fi.com/O4O71FBM0I' target='_blank'>
+      <div className='flex justify-end p-2 block md:hidden sticky bottom-0'>
+        <a
+          href='https://ko-fi.com/O4O71FBM0I'
+          target='_blank'
+        >
           <Image
             height={36}
             width={143}
-            className="h-[36px] w-[143px] border-0"
+            className='h-[36px] w-[143px] border-0'
             src='https://storage.ko-fi.com/cdn/kofi5.png?v=6'
             alt='Buy Me a Coffee at ko-fi.com'
             unoptimized
@@ -176,5 +227,5 @@ return (
         </a>
       </div>
     </div>
-  )
+  );
 }
